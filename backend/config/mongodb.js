cbb55544999faf0701ead164df133130a9773df3
CarlosPulgar <carlos911.cp@gmail.com>
2025-01-13 12:32:32 -0300

    cambio MONGODB_URL / mongoDB_URL

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         
     })
 
-    await mongoose.connect(`${process.env.MONGODB_URL}/e-commerce`)
+    await mongoose.connect(`${process.env.mongoDB_URL}/e-commerce`)
 }
 
 export default connectDB;
